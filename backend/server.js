c340a62020d7f2f9350fe4a6d4b789f1b767c795
Fitsumhelina <permin1501424@gmail.com>
2024-10-16 12:08:14 +0300

    server file cretaed

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// backend/server.js
+const app = require('./app');
+
+// Set the port
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
