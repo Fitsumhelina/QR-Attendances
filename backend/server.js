35c3d7b71021c9657daa8378e3363c407b894cf7
Fitsumhelina <permin1501424@gmail.com>
2024-10-16 12:27:18 +0300

    packae updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const app = require('./app');
 
 // Set the port
 const PORT = process.env.PORT || 5000;
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
