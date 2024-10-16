6dad205fa2ee1d077256e6f504c5581f56e22fd6
Fitsumhelina <permin1501424@gmail.com>
2024-10-16 11:45:35 +0300

    admin model cretaed

---
+++ backend/models/admin.js
@@ -0,0 +1,26 @@
+// backend/models/admin.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Password hashing middleware
+adminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+adminSchema.methods.comparePassword = async function (inputPassword) {
+  return await bcrypt.compare(inputPassword, this.password);
+};
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
